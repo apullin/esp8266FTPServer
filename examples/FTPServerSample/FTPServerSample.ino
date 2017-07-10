f953b0e091f2b793d5c1bd13e8ec52e7b4294642
Andrew Pullin <pullin@berkeley.edu>
2017-07-09 20:54:41 -0700

    Removes unnecessary web server dependency

--- examples/FTPServerSample/FTPServerSample.ino
+++ examples/FTPServerSample/FTPServerSample.ino
@@ -1,35 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <ESP8266FtpServer.h>
 
 const char* ssid = "YOUR_SSID";
 const char* password = "YOUR_PASS";
 
-ESP8266WebServer server(80);
 FtpServer ftpSrv;   //set #define FTP_DEBUG in ESP8266FtpServer.h to see ftp verbose on serial
-
-
-
-void handleRoot() {
-  server.send(200, "text/plain", "hello from esp8266!");
-
-}
-
-void handleNotFound(){
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
 
 void setup(void){
   Serial.begin(115200);
@@ -47,14 +23,6 @@ void setup(void){
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-
-  server.on("/", handleRoot);
-
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
-
   /////FTP Setup, ensure SPIFFS is started before ftp;  /////////
   
   if (SPIFFS.begin()) {
@@ -65,6 +33,5 @@ void setup(void){
 
 void loop(void){
   ftpSrv.handleFTP();        //make sure in loop you call handleFTP()!!  
-  server.handleClient();
  
 }
